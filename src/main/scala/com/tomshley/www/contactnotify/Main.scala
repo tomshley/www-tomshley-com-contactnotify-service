5dfb491b844713201abffa9552a32285a138a49b
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2024-11-21 09:56:22 -0500

    VERSION 0.0.1

--- src/main/scala/com/tomshley/www/contactnotify/Main.scala
+++ src/main/scala/com/tomshley/www/contactnotify/Main.scala
@@ -1,10 +1,17 @@
 package com.tomshley.www.contactnotify
 
 import com.tomshley.hexagonal.lib.ManagedPekkoClusterMain
+import com.tomshley.hexagonal.lib.bootstrap.LoadStatusStartup
 import org.apache.pekko.actor.typed.ActorSystem
+
+import scala.concurrent.ExecutionContext
 
 @main def main(): Unit = {
   ManagedPekkoClusterMain("www-tomshley-com-contactnotify-service", (system: ActorSystem[?]) => {
-    ContactEventConsumer.init(system)
+    LoadStatusStartup.start {
+      given ec:ExecutionContext = system.executionContext
+      
+      ContactEventConsumer.init(system).map(_ => false)
+    }
   })
 }
