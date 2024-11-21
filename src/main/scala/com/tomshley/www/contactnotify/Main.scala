07bd9c775c81434ac6cc98b07dd4c20b56c5a0c4
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2024-11-21 08:16:43 -0500

    VERSION 0.0.1

--- src/main/scala/com/tomshley/www/contactnotify/Main.scala
+++ src/main/scala/com/tomshley/www/contactnotify/Main.scala
@@ -1,12 +1,11 @@
 package com.tomshley.www.contactnotify
 
-import com.tomshley.hexagonal.lib.ManagedActorSystemMain
-import com.tomshley.hexagonal.lib.twilio.util.TwilioClient
-import com.tomshley.hexagonal.lib.twilio.util.TwilioConfig
+import com.tomshley.hexagonal.lib.twilio.util.{TwilioClient, TwilioConfig}
+import com.tomshley.hexagonal.lib.{ManagedActorSystemMain, ManagedPekkoClusterMain}
 import org.apache.pekko.actor.typed.ActorSystem
 
 @main def main(): Unit = {
-  ManagedActorSystemMain("www-tomshley-com-contactnotify-service", (system: ActorSystem[?]) => {
+  ManagedPekkoClusterMain("www-tomshley-com-contactnotify-service", (system: ActorSystem[?]) => {
     TwilioClient.init(TwilioConfig(
       accountSid=system.settings.config.getString("twilio.account_sid"),
       authToken=system.settings.config.getString("twilio.auth_token"),
